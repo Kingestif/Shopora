86c5a6ec05161e1085689bbd820bbceb4ebd93ce
Kingestif <kingestiff@gmail.com>
2025-12-13 23:27:16 +0300

    feat: set up express server with authentication routes

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import authRoutes from './routes/authRoutes.js'
 const app = express()
 
+app.use(express.json())
+
 app.use('/auth', authRoutes)
 
 app.get('/', (req, res)=> {
--- Backend/src/routes/authRoutes.ts
+++ Backend/src/routes/authRoutes.ts
@@ -1,8 +1,9 @@
 import express from 'express'
+import { login } from '../controllers/authControllers/login.js'
+import { signup } from '../controllers/authControllers/signup.js'
 const router = express.Router()
 
-router.get('/login', (req, res)=>{
-    console.log("login route")
-})
+router.post('/login', login)
+router.post('/signup', signup)
 
 export default router
