0dc8bed021f2e474f7c541e84e25999f2c61cd5b
Kingestif <kingestiff@gmail.com>
2025-12-19 13:43:54 +0300

    feat: rename requestPasswordReset controller

--- Backend/src/routes/authRoutes.ts
+++ Backend/src/routes/authRoutes.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import { login } from '../controllers/authControllers/login.js'
 import { signup } from '../controllers/authControllers/signup.js'
 import { emailVerification } from '../controllers/authControllers/emailVerification.js'
-import { requestPasswordReset } from '../controllers/authControllers/resetPassword.js'
+import { requestPasswordReset } from '../controllers/authControllers/requestPasswordReset.js'
 const router = express.Router()
 
 router.post('/login', login)
