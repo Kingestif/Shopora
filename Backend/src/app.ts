1b3f0238a05571114bcd9d9d22f65d279b416f84
Kingestif <kingestiff@gmail.com>
2025-12-19 23:04:05 +0300

    feat: add morgan for logging HTTP requests

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express'
+import morgan from 'morgan'
 import authRoutes from './routes/authRoutes.js'
 import sellerRoutes from './routes/sellerRoutes.js'
 const app = express()
 
 app.use(express.json())
-
+app.use(morgan('dev'))
 app.use('/auth', authRoutes)
 app.use('/seller', sellerRoutes)
 
