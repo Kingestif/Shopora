f7223cccda911d2cf7bd5316146f8ca3f44d4777
Kingestif <kingestiff@gmail.com>
2025-12-20 13:23:32 +0300

    feat: add route to fetch products for admin

--- Backend/src/routes/adminRoutes.ts
+++ Backend/src/routes/adminRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { getUsers } from "../controllers/adminControllers/getUsers.js";
+import { getProducts } from "../controllers/buyerController/getProducts.js";
 
 const router = Router()
 
 router.get('/users', getUsers)
+router.get('/products', getProducts)
 
 export default router;
