ee11df6bc4e6241d8a106dcbd511c5779e4c9a3c
Kingestif <kingestiff@gmail.com>
2025-12-24 00:28:00 +0300

    feat: improve layout structure to make footer stays at bottom

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,14 +1,14 @@
 import Navbar from "@/components/ui/Navbar";
 import { Metadata } from "next";
-import './globals.css';
+import "./globals.css";
 import Footer from "@/components/ui/Footer";
 
 export const metadata: Metadata = {
   title: "Shopora",
   description: "Shopping made simple",
   icons: {
-    icon: '/favicon.png'
-  }
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
@@ -18,10 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <Navbar/>
-        {children}
-        <Footer/>
+      <body className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-1">{children}</main>
+        <Footer />
       </body>
     </html>
   );
